6b3aef25c946886d1972c17d08cd16b9d52445fa
andrei_kiashev <andreeyy49@gmail.com>
2025-02-23 22:32:38 +0300

    create community-service

---
+++ community-service/src/test/java/voteapp/communityservice/CommunityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package voteapp.communityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
